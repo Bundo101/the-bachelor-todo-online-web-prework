10f5584c34a0f9cbf32f7d73faa825ceac78f489
James Britton <brittonjp@gmail.com>
2019-05-06 15:53:34 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -35,7 +35,13 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season_numbers, contestant_arrays|
+    contestant_arrays.each do |contestant|
+      if contestant_arrays["hometown"] == hometown
+        return contestant_arrays["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
